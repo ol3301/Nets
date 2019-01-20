75338f2de7d9a37420cf58cb618b69855ea07ed8
Mazda <ol.kyzy@gmail.com>
2019-01-20 11:27:53 +0200

    exam

---
+++ NetExamChatClient/NetExamChatClient/Helpers.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExamChatClient
+{
+    public class Com
+    {
+        public const string NewServer = "NewServer";
+        public const string NewClient = "NewClient";
+        public const string AuthOk = "AuthOk";
+        public const string AuthNo = "AuthNo";
+        public const string NotServer = "NotServer";
+        public const string ServerOnline = "ServerOnline";
+
+        public const string NewSession = "NewSession";
+        public const string DestroyTransfer = "DestroyTransfer";
+        public const string DestroySession = "DestroySession";
+        public const string NewTransfer = "NewTransfer";
+        public const string UpdateTransfer = "UpdateTransfer";
+        public const string TransferReady = "TransferReady";
+        public const string RemoveTransfer = "RemoveTransfer";
+
+        //
+
+        public const string ActiveUsers = "ActiveUsers";
+        public const string MessText = "MessText";
+    }
+
+    class StructMessage
+    {
+        public string title { get; set; }
+        public string type { get; set; } = "Null";
+        public Dictionary<string, string> param { get; set; }
+
+        public StructMessage(string title, string type)
+        {
+            this.title = title;
+            this.type = type;
+
+            param = new Dictionary<string, string>();
+        }
+        public StructMessage(string data)
+        {
+            title = Help.FindText(ref data, ',');
+            type = Help.FindText(ref data, ',');
+
+            if (data.Length > 0)
+                param = new Dictionary<string, string>(
+                    data.Split(';')
+                    .Select(part => part.Split('='))
+                    .Where(part => part.Length == 2)
+                    .ToDictionary(sp => sp[0], sp => sp[1]));
+            else
+                param = new Dictionary<string, string>();
+        }
+        public StructMessage()
+        {
+            param = new Dictionary<string, string>();
+        }
+
+        public void AddParam(string key, string value)
+        {
+            param.Add(key, value);
+        }
+        public void RemoveParam(string key)
+        {
+            param.Remove(key);
+        }
+        public override string ToString()
+        {
+            string par = "";
+            foreach (KeyValuePair<string, string> i in param)
+                par += i.Key + "=" + i.Value + ";";
+
+            return $"{title},{type},{par}";
+        }
+
+        public string this[string i]
+        {
+            get { return param[i]; }
+            set { param[i] = value; }
+        }
+    }
+
+    class Help
+    {
+        static Random rnd = new Random();
+        public static string GenId(int len)
+        {
+            string res = "";
+            for (int i = 0; i < len; ++i)
+                res += rnd.Next(9).ToString();
+            return res;
+        }
+        public static string FindText(ref string buf, char ch)
+        {
+            int pos = buf.IndexOf(ch);
+            string res = "";
+            if (pos > 0)
+            {
+                res = buf.Substring(0, pos);
+                buf = buf.Remove(0, pos + 1);
+            }
+            return res;
+        }
+    }
+}
---
+++ NetExamChatClient/NetExamChatClient/VM.cs
@@ -0,0 +1,144 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NetExamChatClient
+{
+    public class VM : BindableBase
+    {
+        private TcpClient client;
+        
+
+        public string Ip { get; set; }
+        public int Port { get; set; }
+        public string Name { get; set; }
+        public string TextArea { get; set; }
+        public string CurentMess { get; set; }
+        public string Pass { get; set; }
+        public ObservableCollection<string> Users { get; set; }
+        private DelegateCommand _ConnectCommand;
+        private DelegateCommand _SendCommand;
+
+        public DelegateCommand SendCommand
+        {
+            get => _SendCommand ?? (_SendCommand = new DelegateCommand(async()=>
+            {
+                StructMessage mess = new StructMessage();
+                mess.title = Com.MessText;
+                mess.param["sender"] = Name;
+                mess.param["mess"] = CurentMess;
+                AppendArea(Name+": "+CurentMess);
+                CurentMess = "";
+                await SendStruct(mess);
+            }));
+        }
+        public DelegateCommand ConnectCommand
+        {
+            get => _ConnectCommand ?? (_ConnectCommand = new DelegateCommand(async ()=>
+            {
+                try
+                {
+                    if (!client.Connected)
+                        await client.ConnectAsync(IPAddress.Parse(Ip), Port);
+                    await SignIn();
+                    Reading();
+                }
+                catch(Exception ex)
+                {
+                    AppendArea("Ошибка. "+ex.Message);
+                }
+            }));
+        }
+
+        public VM()
+        {
+            client = new TcpClient();
+            Users = new ObservableCollection<string>();
+            Ip = "127.0.0.1";
+            Port = 2020;
+            Name = "1";
+            Pass = "2";
+        }
+
+        private async Task SignIn()
+        {
+            StructMessage mess = new StructMessage();
+            mess.title = Com.NewServer;
+            mess["login"] = Name;
+            mess["passw"] = Pass;
+            await SendStruct(mess);
+        }
+
+        private void AppendArea(string text)
+        {
+            TextArea += text + "\n";
+        }
+
+        private async Task SendStruct(StructMessage mess)
+        {
+            await SendText(mess.ToString());
+        }
+
+        private async Task SendText(string text)
+        {
+            byte[] buff = Encoding.UTF8.GetBytes(text + "@");
+            await SendBuff(buff);
+        }
+
+        private async Task SendBuff(byte[] buff)
+        {
+            NetworkStream stream = client.GetStream();            
+            await stream.WriteAsync(buff, 0, buff.Length);           
+        }
+
+        private async void Reading()
+        {
+            NetworkStream stream = client.GetStream();          
+            await Task.Run(async () =>
+            {
+                while (client.Connected)
+                {
+                    byte[] buff = new byte[client.ReceiveBufferSize];
+                    int bytes = await stream.ReadAsync(buff, 0, buff.Length);
+                    await CommandWorking(new StructMessage(Encoding.UTF8.GetString(buff, 0, bytes)));
+                }
+            });
+        }
+
+        private async Task CommandWorking(StructMessage mess)
+        {
+            if(mess.title == Com.NewServer)
+            {
+                if (mess.type == Com.AuthOk)
+                    AppendArea("Подключены!");
+                else if (mess.type == Com.AuthNo)
+                    AppendArea("Не правельный логин или пароль");
+                else if (mess.type == Com.ServerOnline)
+                    AppendArea("В данный аккаунт уже выполнен вход");
+            }
+            else if(mess.title == Com.ActiveUsers)
+            {
+                Application.Current.Dispatcher.Invoke(new Action(()=>
+                {
+                    Users.Clear();
+                    foreach (var str in mess.param)
+                    {
+                        Users.Add(str.Key);
+                    }
+                }));
+            }
+            else if(mess.title == Com.MessText)
+            {
+                AppendArea($"{mess["sender"]}: {mess["mess"]}");
+            }
+        }
+    }
+}
